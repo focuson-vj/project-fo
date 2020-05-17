8f40e306b75af5f01dc2f344578ebd8fe1596f4a
sixd <nakazatokenya@gmail.com>
2020-05-18 00:12:34 +0900

    Vuetifyの追加と簡単なコントローラページ(見た目だけ)の作成

--- src/components/Controller.vue
+++ src/components/Controller.vue
@@ -1,30 +1,41 @@
 <template>
   <v-container>
     <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-
-      <v-col class="mb-4">
+      <v-col class="mb-4" cols="12">
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
+          Welcome to Focus On!
         </h1>
 
         <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank"
-            >Discord Community</a
-          >
+          Interactive VJ System for Focus On!
         </p>
       </v-col>
 
-      <v-col class="mb-5" cols="12">
+      <v-col cols="3">
+        <v-btn class="mx-2" fab dark small color="pink">
+          <v-icon dark>mdi-heart</v-icon>
+        </v-btn>
+      </v-col>
+
+      <v-col cols="3">
+        <v-btn class="mx-2" fab dark small color="green">
+          <v-icon dark>mdi-heart</v-icon>
+        </v-btn>
+      </v-col>
+
+      <v-col cols="3">
+        <v-btn class="mx-2" fab dark small color="blue">
+          <v-icon dark>mdi-heart</v-icon>
+        </v-btn>
+      </v-col>
+
+      <v-col cols="3">
+        <v-btn class="mx-2" fab dark small color="yellow">
+          <v-icon dark>mdi-heart</v-icon>
+        </v-btn>
+      </v-col>
+
+      <!-- <v-col class="mb-5" cols="12">
         <h2 class="headline font-weight-bold mb-3">
           What's next?
         </h2>
@@ -76,14 +87,14 @@
             {{ eco.text }}
           </a>
         </v-row>
-      </v-col>
+      </v-col> -->
     </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Controller',
 
   data: () => ({
     ecosystem: [
