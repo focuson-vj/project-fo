0a9b78ba353039722894773a86a0fbe4df4e8745
sixd <nakazatokenya@gmail.com>
2020-05-18 19:30:49 +0900

    コントローラページにVuexを紐付けた

--- src/components/Controller.vue
+++ src/components/Controller.vue
@@ -11,143 +11,46 @@
         </p>
       </v-col>
 
-      <v-col cols="3">
-        <v-btn class="mx-2" fab dark small color="pink">
+      <v-col v-for="(button, index) in buttons" :key="index" cols="3">
+        <v-btn
+          class="mx-2"
+          dark
+          small
+          :color="button.color"
+          @click="increment(index)"
+        >
           <v-icon dark>mdi-heart</v-icon>
+          <span>{{ count[index] }}</span>
         </v-btn>
       </v-col>
-
-      <v-col cols="3">
-        <v-btn class="mx-2" fab dark small color="green">
-          <v-icon dark>mdi-heart</v-icon>
-        </v-btn>
-      </v-col>
-
-      <v-col cols="3">
-        <v-btn class="mx-2" fab dark small color="blue">
-          <v-icon dark>mdi-heart</v-icon>
-        </v-btn>
-      </v-col>
-
-      <v-col cols="3">
-        <v-btn class="mx-2" fab dark small color="yellow">
-          <v-icon dark>mdi-heart</v-icon>
-        </v-btn>
-      </v-col>
-
-      <!-- <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col> -->
     </v-row>
   </v-container>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   name: 'Controller',
 
   data: () => ({
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader',
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify',
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
-      },
-    ],
-    importantLinks: [
-      {
-        text: 'Documentation',
-        href: 'https://vuetifyjs.com',
-      },
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com/components/api-explorer',
-      },
-      {
-        text: 'Select a layout',
-        href: 'https://vuetifyjs.com/layout/pre-defined',
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href:
-          'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
+    buttons: [
+      { color: 'pink' },
+      { color: 'green' },
+      { color: 'orange' },
+      { color: 'blue' },
     ],
   }),
+  computed: {
+    ...mapState(['count']),
+  },
+  methods: {
+    increment(index) {
+      this.$store.dispatch('increment', index)
+    },
+  },
+  beforeMount: function() {
+    this.$store.dispatch('init', 4)
+  },
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,30 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+  modules: {},
+  state: {
+    count: [],
+  },
+  mutations: {
+    increment: (state, { index, amount }) => {
+      Vue.set(state.count, index, state.count[index] + amount)
+    },
+    init: (state, length) => {
+      console.log('init', length)
+      for (let i = 0; i < length; i++) {
+        state.count.push(0)
+      }
+    },
+  },
+  actions: {
+    increment: ({ commit }, index) => {
+      commit('increment', { index, amount: 1 })
+    },
+    init: ({ commit }, length) => {
+      commit('init', length)
+    },
+  },
+})
