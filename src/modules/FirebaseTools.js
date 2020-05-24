58d311e91f8394972a05c5719f68160625b4b7a8
k2font <shoichiro.kouno@gmail.com>
2020-05-24 11:16:18 +0900

    Some changes

--- src/modules/FirebaseTools.js
+++ src/modules/FirebaseTools.js
@@ -2,7 +2,7 @@
 
 import { firebase, db } from "../firebase";
 
-const FirebaseTools = (_id, doc_id) => {
+const IncrementParam = (_id, doc_id) => {
   let counterRef = db.collection(_id).doc(doc_id);
   counterRef
     .update({
@@ -18,4 +18,26 @@ const FirebaseTools = (_id, doc_id) => {
     });
 };
 
-export default FirebaseTools;
+// エフェクトを変更する関数
+// 押すボタンによってFirebaseに格納される値が変化する
+const ChangeEffect = () => {
+  return 0;
+};
+
+// スライダー
+const Slider = () => {
+  return 0;
+};
+
+// bpm情報を待ち受けてonSnapshotで受け取って
+// 値をstoreに格納する関数
+const bpmReceiver = () => {
+  return 0;
+};
+
+export default {
+  IncrementParam,
+  ChangeEffect,
+  Slider,
+  bpmReceiver
+};
