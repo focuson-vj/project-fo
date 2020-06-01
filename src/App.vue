7cb650a74327396f6d4f4be6b02377d16373c289
sixd <nakazatokenya@gmail.com>
2020-06-02 02:06:24 +0900

    ナブゲーション追加  基本的な画面の構造が感性

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,44 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-navigation-drawer
+      app
+      v-model="drawer"
+      cliped-right
+      overlay
+      bottom
+      dark
+      dense
+    >
+      <v-list-item>
+        <v-list-item-title class="title">
+          Links
+        </v-list-item-title>
+        <v-btn icon>
+          <v-icon @click.stop="drawer = !drawer">mdi-chevron-down</v-icon>
+        </v-btn>
+      </v-list-item>
+      <v-divider />
+      <v-list nav>
+        <v-list-item v-for="menu in menus" :key="menu.title" :to="menu.url">
+          <v-list-item-icon>
+            <v-icon>{{ menu.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ menu.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
-        <v-toolbar-title>Focus On!</v-toolbar-title>
-      </div>
-
-      <v-spacer></v-spacer>
+    <v-app-bar app right dark>
+      <!-- アプリケーションバー -->
+      <v-avatar>
+        <img src=".\assets\focuson_logo.jpg" alt="Logo of Focus On!" />
+      </v-avatar>
+      <v-toolbar-title style=" margin: 0 auto;"
+        >Interactive VJ System</v-toolbar-title
+      >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
     </v-app-bar>
 
     <v-content>
@@ -37,7 +61,18 @@ export default {
   },
 
   data: () => ({
-    //
+    drawer: false,
+    // リンク一覧
+    menus: [
+      {
+        title: 'Twitter',
+        icon: ' mdi-twitter',
+        url: 'https://twitter.com/Focus_on_dj',
+      },
+      { title: 'Home', icon: 'mdi-home', url: '/home' },
+      { title: 'Favorites', icon: 'mdi-heart', url: '/favorites' },
+      { title: 'About', icon: 'mdi-information-variant', url: '/about' },
+    ],
   }),
 }
 </script>
